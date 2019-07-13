58517a054a05b4a50f6c35091de3f29c2ddc1623
Albert <gumarov2017@yndex.ru>
2019-07-13 10:36:20 +0600

    шаг 3,5

--- src/cli.php
+++ src/cli.php
@@ -5,7 +5,7 @@ use function \cli\line;
 use function \cli\prompt;
 
 function run()
-{
+{   
     line('Welcome to the Brain Game!');
     $playerName = prompt('May I have your name?');
     line("Hello, %s!", $playerName);
